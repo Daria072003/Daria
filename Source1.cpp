6c431e5052879340e87ee5b4486f5585e0c3a498
Daria072003 <92159266+Daria072003@users.noreply.github.com>
2021-10-13 22:06:52 +0300

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main() {
+    int x;
+    std::cin >> x;
+    int x2 = x;
+    for (int i = 1; i <= x; ++i) {
+        for (int k = (x2 - 1); k >= 1;--k) {
+            std::cout << " ";
+        }
+        --x2;
+        for (int j = 1; j <= (i * 2 - 1); ++j) {
+            std::cout  << "*";
+        }
+        std::cout << "\n";
+    }
+}
