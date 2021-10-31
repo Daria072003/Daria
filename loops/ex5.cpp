473516eace11a01691038b6986870af61c5c8bfd
Daria <92159266+Daria072003@users.noreply.github.com>
2021-10-31 16:17:23 +0300

    Create ex5.cpp

---
+++ loops/ex5.cpp
@@ -0,0 +1,21 @@
+  
+#include <iostream>
+int main(){
+    double x1, x2, dx, eps;
+    std::cout << "x1 x2 dx eps: ";
+    std::cin >> x1 >> x2 >> dx >> eps;
+    std::cout << "x\tf(x)\t\tsin(x)\n";
+    for (; x1 <= x2; x1 += dx){
+        std::cout << x1 << "\t";
+        double curr = x1;
+        double sum = 0;
+        int cnt = 1;
+        while (std::abs(curr) >= eps){
+            sum += curr;
+            cnt += 2;
+            curr = -curr * x1 * x1 / (cnt * (cnt - 1));
+        }
+        std::cout << sum << "\t" << sin(x1) << "\n";
+    }
+    return 0;
+}
