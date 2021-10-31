0fb01acce42736aa2826c0af4624b54571c6280d
Daria <92159266+Daria072003@users.noreply.github.com>
2021-10-31 16:19:01 +0300

    Create ex9.cpp

---
+++ loops/ex9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main(){
+    int num;
+    std::cout << "num = ";
+    std::cin >> num;
+    int num_tmp = num;
+    int deg = 0;
+    while ((num_tmp /= 2) != 0)
+        ++deg;
+    int two_deg = 1 << deg;
+    while (two_deg != 0) {
+        std::cout << (num / two_deg);
+        num %= two_deg;
+        two_deg >>= 1;
+    }
+    std::cout << "\n";
+    return 0;
+}
