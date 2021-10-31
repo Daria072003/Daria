d04a1332994c54de102ecd872cb8ca86ef162b36
Daria <92159266+Daria072003@users.noreply.github.com>
2021-10-31 11:41:57 +0300

    Create ex1.cpp

---
+++ array/ex1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int main() {
+    int First[3];
+    int Second[3];
+    int Result[3];
+    for (int i = 0; i < 3; ++i) {
+        std::cin >> First[i];
+    }
+    for (int i = 0; i < 3; ++i){
+        std::cin >> Second[i];
+        Result[i] = Second[i]-First[i];
+    }
+    for (int i = 1; i < 3; ++i) {
+        if (Result[i] < 0) {
+            Result[i - 1] -= 1;
+            Result[i] = 60 + Result[i];
+        }
+    }
+    for (int i = 0; i < 3; ++i) {
+        std::cout << Result[i] << " ";
+    }
+}
