ff36b4dd1c67bf243c3a62053612f5f7eedfffc2
Daria <92159266+Daria072003@users.noreply.github.com>
2021-10-31 13:22:11 +0300

    Update ex3.cpp

--- array/ex3.cpp
+++ array/ex3.cpp
@@ -4,10 +4,12 @@ int main() {
     int N;
     char OST[1000];
     int k = 0;
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите десятичное число>";
     std::cin >> N;
     while (N != 0) {
         k = k + 1;
-        if ((N % 16) < 10) {    
+        if ((N % 16) < 10) {
             OST[k] = '0' + N % 16;
         }
         else {
@@ -15,7 +17,23 @@ int main() {
         }
         N = N / 16;
     }
+    std::cout << "Число в 16-ой системе счисления = ";
     for (int i = k; i >= 1; --i) {
         std::cout << OST[i];
     }
+    std::cout << "\n";
+    std::string STR;
+    std::cout << "Введите число в 16-ой системе счисления>";
+    std::cin >> STR;
+    int S = 0;
+    for (int i = 0; i < STR.length(); ++i) {
+        if (STR[i] >= 'A') {
+            STR[i] = STR[i] - 55;
+            S += int(STR[i])*pow(16,STR.length()- (i+1));
+        }
+        else {
+            S += (STR[i] - '0')* pow(16, STR.length() - (i+1));
+        }
+    }
+    std::cout << "Число в 10-ой системе счисления = " << S;
 }
