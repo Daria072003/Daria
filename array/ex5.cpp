385f0effc6b417a1132169e6b97191177344ac89
Daria <92159266+Daria072003@users.noreply.github.com>
2021-10-31 16:08:08 +0300

    Create ex5.cpp

---
+++ array/ex5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main() {
+    int N;
+    int A[1000];
+    std::cin >> N;
+    for (int i = 1; i <= N; ++i) {
+        std::cin >> i[A];
+    }
+    int S = 0;
+    int ind = 0;
+    for (int i = 1; i <= N; ++i) {
+        if ((A[i] + A[i + 1]) > S) {
+            S = A[i] + A[i + 1];
+            ind = i;
+        }
+    }
+    std::cout << A[ind] << " " << A[ind + 1];
+}
