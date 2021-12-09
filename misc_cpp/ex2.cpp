0d8b7d20221c091111ebba01d70669ad39a8ec91
Daria <92159266+Daria072003@users.noreply.github.com>
2021-12-09 16:18:30 +0300

    Create ex2.cpp

---
+++ misc_cpp/ex2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+int main() {
+	setlocale(LC_ALL, "russian");
+	float a, b, c;
+	std::cout << "Input a,b,c\n";
+	std::cin >> a >> b >> c;
+	float d = b * b - 4 * a * c;
+	if (d < 0)
+	{
+		std::cout << "Нет корней";
+	}
+	else if(d==0)
+	{
+		std::cout << "Уравнение имеет 1 корень: " << -b / 2 / a;
+	}
+	else 
+	{
+		float x1 = (-b + sqrt(d)) / 2 / a;
+		float x2 = (-b - sqrt(d)) / 2 / a;
+		std::cout << "Уравнение имеет 2 корня: " << x1 << " и " << x2;
+	}
+}
