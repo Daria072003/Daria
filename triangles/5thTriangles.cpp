45b322abb0d152d0ac441b5656e8170cf16d4583
Daria <92159266+Daria072003@users.noreply.github.com>
2021-12-10 20:42:57 +0300

    Update 5thTriangles.cpp

--- triangles/5thTriangles.cpp
+++ triangles/5thTriangles.cpp
@@ -3,10 +3,9 @@
 int main() {
     int x, N;
     std::cin >> N;
-    x = N / 2 + 1;
-    int x2 = x;
-    for (int i = 1; i <= x; ++i) {
-        for (int k = (x2 - 1); k >= 1;--k) {
+    int x2 = N;
+    for (int i = 1; i <= N; ++i) {
+        for (int k = (x2 - 1); k >= 1; --k) {
             std::cout << " ";
         }
         --x2;
@@ -21,9 +20,9 @@ int main() {
         std::cout << "\n";
     }
     x2 = 1;
-    for (int i = (x - 1); i >= 1; --i) {
+    for (int i = (N - 1); i >= 1; --i) {
         for (int k = 1; k <= x2; ++k) {
-            std::cout  << " ";
+            std::cout << " ";
         }
         ++x2;
         for (int j = 1; j <= (i * 2 - 1); ++j) {
