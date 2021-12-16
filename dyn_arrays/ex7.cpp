66d2bb99215e31a606be252c21048aa3483eed8f
Daria <92159266+Daria072003@users.noreply.github.com>
2021-12-16 19:11:35 +0300

    Create ex7.cpp

---
+++ dyn_arrays/ex7.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+void shiftArray(int* A, int n, int k) {
+    for (int i = 0; i < n; ++i) {
+        if (i < k) {
+            A[n - k + i] = i;
+        }
+        else {
+            A[i - k] = i;
+        }
+    }
+}
+
+int main() {
+    int N, z;
+    std::cin >> N >> z;
+    int* arr = new int [N];
+    for (int i = 0; i < N; ++i) {
+        arr[i] = i;
+    }
+    shiftArray(arr, N, z);
+    for (int i = 0; i < N; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    delete[] arr;
+}
