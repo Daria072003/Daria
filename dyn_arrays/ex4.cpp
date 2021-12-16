9051258e96136b9609a4d11e2d5739c3a7c743d5
Daria <92159266+Daria072003@users.noreply.github.com>
2021-12-16 17:48:11 +0300

    Create ex4.cpp

---
+++ dyn_arrays/ex4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+
+double mysqrt(double x, double eps = 1e-6) {
+    double curr = 1.0;
+    double next = curr - (curr * curr - x) / (2 * curr);
+    while (std::abs(curr - next) > eps) {
+        curr = next;
+        next = curr - (curr * curr - x) / (2 * curr);
+    }
+    return next;
+}
+
+int main() {
+    int X;
+    std::cin >> X;
+    std::cout << mysqrt(X);
+}
