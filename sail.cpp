27f0421c18309cfb968bb770c466239b3820bd28
Daria <92159266+Daria072003@users.noreply.github.com>
2021-12-17 12:50:38 +0300

    Update sail.cpp

--- sail.cpp
+++ sail.cpp
@@ -5,10 +5,14 @@ int main() {
     std::cin >> x;
     std::cin >> y;
 
-    if (y < 0 && (((x - 1) * (x - 1) + y * y <= 1)) || ((x + 1) * (x + 1) + y * y <= 1))
+    if (y < 0 && (((x - 1) * (x - 1) + y * y <= 1)) || ((x + 1) * (x + 1) + y * y <= 1)){
         std::cout << "Yes" << std::endl;
-    else if (y >= 0 && (y <= 2 - x * 2.) && (y <= 2 + 2 * x))
+    }
+    else if (y >= 0 && (y <= 2 - x * 2.) && (y <= 2 + 2 * x)){
         std::cout << "Yes" << std::endl;
-    else
+    }
+    else{
         std::cout << "No" << std::endl;
+    }
+    return 0;
 }
