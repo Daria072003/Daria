6dd85f65f36e6e8015d7802b6ad1098cec5cffb9
Daria <92159266+Daria072003@users.noreply.github.com>
2021-12-17 10:00:11 +0300

    Update ex8.cpp

--- misc_cpp/ex8.cpp
+++ misc_cpp/ex8.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 
 int main() {
-    for(i=1; i<=100; ++i){
-        if (i%3 == 0 ){
-            std::cout << "Foo";
+    for (int i = 1; i <= 100; ++i) {
+        if (i % 3 == 0 & i % 5 == 0) {
+            std::cout << "FooBar" << "\n";
         }
-        else if (i%5 == 0 ){
-            std::cout << "Bar";
+        else if (i % 3 == 0) {
+            std::cout << "Foo" << "\n";
         }
-        else if (i%3 == 0 & i%5 == 0){
-            std::cout << "FooBar";
+        else if (i % 5 == 0) {
+            std::cout << "Bar" << "\n";
         }
-        else{
-            std::cout << i;
+        else {
+            std::cout << i << "\n";
         }
     }
 }
