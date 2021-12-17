440c82f3324feb81e667bb92a70410cc3a1910d4
Daria <92159266+Daria072003@users.noreply.github.com>
2021-12-17 13:15:47 +0300

    Update ex5.cpp

--- misc_cpp/ex5.cpp
+++ misc_cpp/ex5.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 int main(){
-    float a, b, c, d, x;
+    double a, b, c, d, x;
     std::cin >> a >> b >> c >> d >> x;
-    float y = 0;
+    double y = 0.0;
     y = c + (x - a) * (d - c) / (b - a);
     std::cout << y;
     return 0;
